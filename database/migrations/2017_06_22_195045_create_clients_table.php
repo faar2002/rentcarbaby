a46d41274606664bf06f2e27a7a1dec23d70e9de
faar2002 <faar2002@gmail.com>
2017-06-22 20:51:23 -0300

    Nuevo archivo para crear la taba de client

---
+++ database/migrations/2017_06_22_195045_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',60);
+            $table->string('rut',11)->unique();
+            $table->string('email',40)->unique();
+            $table->integer('phone');
+            $table->string('address',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
